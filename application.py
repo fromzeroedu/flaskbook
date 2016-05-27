2799ac8cf4bcba5d8509f79cc8735a777c75f211
From Zero <jorge@fromzero.io>
2016-05-27 19:20:32 +0000

    completed step-4

--- application.py
+++ application.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+    
+    db.init_app(app)
     
     from user.views import user_app
     app.register_blueprint(user_app)
---
+++ user/models.py
@@ -0,0 +1,11 @@
+from application import db
+
+from utilities.common import utc_now_ts as now
+
+class User(db.Document):
+    username = db.StringField(db_field="u", required=True)
+    password = db.StringField(db_field="p", required=True)
+    email = db.EmailField(db_field="e", required=True)
+    first_name = db.StringField(db_field="fn", max_length=50)
+    last_name = db.StringField(db_field="ln", max_length=50)
+    created = db.IntField(db_field="c", default=now())
