d7910096a7ba2f56470ea82fe3ca2c50407defa0
From Zero <jorge@fromzero.io>
2016-06-24 17:13:59 +0000

    completed step-9

--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, url_for
 import bcrypt
 
 from user.models import User
@@ -49,3 +49,12 @@ def login():
         if not user:
             error = 'Incorrect credentials'
     return render_template('user/login.html', form=form, error=error)
+    
+@user_app.route('/logout', methods=('GET', 'POST'))
+def logout():
+    session.pop('username')
+    return redirect(url_for('user_app.login'))
+    
+@user_app.route('/profile', methods=('GET', 'POST'))
+def profile():
+    return render_template('user/profile.html')
