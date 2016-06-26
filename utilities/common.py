c24cb7eef0a928b75c68c023f92e45a2a6e1311b
From Zero <jorge@fromzero.io>
2016-06-26 20:55:59 +0000

    finished step-12

--- utilities/common.py
+++ utilities/common.py
@@ -1,4 +1,32 @@
 import time
+import boto3
 
 def utc_now_ts():
     return int(time.time())
+    
+def email(to_email, subject, body_html, body_text):
+    client = boto3.client('ses')
+    return client.send_email(
+        Source='webmaster@fromzero.io',
+            Destination={
+                'ToAddresses': [
+                    to_email,
+                ]
+            },
+        Message={
+            'Subject': {
+                'Data': subject,
+                'Charset': 'UTF-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': body_text,
+                    'Charset': 'UTF-8'
+                },
+                'Html': {
+                    'Data': body_html,
+                    'Charset': 'UTF-8'
+                },                
+            }
+        }
+    )
